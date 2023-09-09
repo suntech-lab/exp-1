6a2e5911ba8cad462dc52a713280dc87b3d60562
Eric <eric@suntech.com>
2023-09-09 15:05:04 -0700

    Add.

--- PrimeNumberFunder.py
+++ PrimeNumberFunder.py
@@ -1,8 +1,10 @@
-n = 0
+n = 3
 while n < 100000:
-    for x in range(2, n):
+    x = 2
+    while x < n:
         if n % x == 0:
             break
+        x += 1
     else:
         print(n)
     n += 1
