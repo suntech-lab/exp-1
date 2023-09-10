084302672f83efd0d9878e4e5e6b6ea9065fb298
motbbs <144563383+motbbs@users.noreply.github.com>
2023-09-10 16:29:57 -0700

    Update smallesttolargest.py

--- smallesttolargest.py
+++ smallesttolargest.py
@@ -8,6 +8,19 @@ def gen_list(size):
         i += 1
     return list
 
+def sort_list(list):
+    n = len(list)
+    swapped = false
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if list[j] > list[j + 1]:
+                swapped = true
+                list[j], list[j + 1] = list[j + 1], list[j]
+        if not swapped:
+            return
+    
+
+    
 def sorted_list(arg):
     list = gen_list(10)
     max = 0
