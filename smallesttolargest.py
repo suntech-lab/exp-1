1e17f731b9791ef2bbd89e12f4141deb03eb8cff
Eric <eric@suntech.com>
2023-09-10 16:39:11 -0700

    Add.

--- smallesttolargest.py
+++ smallesttolargest.py
@@ -4,31 +4,21 @@ def gen_list(size):
     i = 0
     list = []
     while i < size:
-        list.append(random.randint(0, 10))
+        list.append(random.randint(0, 100))
         i += 1
     return list
 
 def sort_list(list):
+    list = gen_list(20)
     n = len(list)
-    swapped = false
+    swapped = False
     for i in range(n-1):
         for j in range(0, n-i-1):
             if list[j] > list[j + 1]:
-                swapped = true
+                swapped = True
                 list[j], list[j + 1] = list[j + 1], list[j]
         if not swapped:
             return
-    
 
-    
-def sorted_list(arg):
-    list = gen_list(10)
-    max = 0
-    i = 0
-    while i < len(list):
-        for i in list:
-            if max < list[i]:
-                max = list[i]
-                i += 1
-            else:
-                i += 1
+sort_list(list)
+print(list)
