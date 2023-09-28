40d6a4a456200a168cc5a39518f264d5881d2340
Eric <ericliu3488@gmail.com>
2023-09-28 13:32:38 -0700

    Add.

---
+++ binaryadder.py
@@ -0,0 +1,7 @@
+import random
+
+list = [[],[],[]
+        [],[],[]]
+
+def gen_binary(arg)
+https://cdn.discordapp.com/attachments/803410379950522379/1157045639344042085/IMG_0919.png?ex=65172e39&is=6515dcb9&hm=61717add490b47277f9d0c1304e833994fe4e57464897f02ee5d3033f3b53647&
