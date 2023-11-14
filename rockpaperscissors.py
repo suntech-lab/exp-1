ee49acb69b5c3567610027f2b3509f19cb829332
Eric <ericliu3488@gmail.com>
2023-11-14 09:42:18 -0800

    Add.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,8 +2,30 @@ import random
 n = str(input('What is your choice?:'))
 i = random.randint(1,4)
 if i == 1:
-    print('rock')
+    print('i choose rock')
 if i == 2:
-    print('paper')
+    print('i choose paper')
 if i == 3:
-    print('scissors')
+    print('i choose scissors')
+
+if n == 'rock':
+    if i == 1:
+        print('tie')
+    if i == 2:
+        print('you lose')
+    if i == 3:
+        print('you win')
+if n == 'paper':
+    if i == 1:
+        print('you win')
+    if i == 2:
+        print('tie')
+    if i == 3:
+        print('you lose')
+if n == 'scissors':
+    if i == 1:
+        print('you lose')
+    if i == 2:
+        print('you win')
+    if i == 3:
+        print('tie')
