171bf7fc81ba8d4d295b37894789505530952cfe
Eric <ericliu3488@gmail.com>
2023-11-20 10:58:46 -0800

    Add.

--- session2.py
+++ session2.py
@@ -1,31 +1,50 @@
-num1 = int(input("Enter the first number: "))
+# import math
+import math
 
-def factorial(int):
-    x = 1
-    while num1 > 0:
-        fact = (num1 - x) * num1
-    x += 1
-    return fact
+x = str(input('functions (limited) or arithmetic operations: '))
 
-op = str(input("Enter the operation: "))
+if x == 'arithmetic operations':
+    num1 = complex(input('enter the first number: '))
+    op = str(input('enter the operation: '))
+    num2 = complex(input('enter the second number: '))
 
-if op == "factorial":
-    print(num1, "factorial is ", factorial(num1))
+    if op == "+":
+        print(num1, "+", num2, "is", num1 + num2)
+    if op == "-":       
+        print(num1, "-", num2, "is", num1 - num2)
+    if op == "/":
+        print(num1, "/", num2, "is", num1 / num2)
+    if op == "*":
+        print(num1, "*", num2, "is", num1 * num2)
+    if op == "%":
+        print(num1, "%", num2, "is", num1 % num2)
+    if op == "//":
+        print(num1, "//", num2, "is", num1 // num2)
+    if op == "**":
+        print(num1, "**", num2, "is", num1 ** num2)
 
-num2 = int(input("Enter the second number: "))
+if x == 'functions':
+    op = str(input('factorial, delta, square root, or absolute value: '))
 
-if op == "+":
-    print(num1, "+", num2, "is", num1 + num2)
-if op == "-":       
-    print(num1, "-", num2, "is", num1 - num2)
-if op == "/":
-    print(num1, "/", num2, "is", num1 / num2)
-if op == "*":
-    print(num1, "*", num2, "is", num1 * num2)
-if op == "%":
-    print(num1, "%", num2, "is", num1 % num2)
-if op == "//":
-    print(num1, "//", num2, "is", num1 // num2)
-if op == "**":
-    print(num1, "**", num2, "is", num1 ** num2)
+    if op == 'factorial':
+        factorial = 1
+        num1 = int(input('what number do you want the factorial of? (has to be a positive integer): '))
+        for i in range(1, num1 + 1):
+            factorial = factorial * i
+        print('the factorial of', num1, 'is', factorial)
+    if op == 'delta':
+        delta = 0
+        num1 = int(input('what do you want the delta of (has to be a positive integer): '))
+        for i in range(1, num1 + 1):
+            delta = delta + i
+        print('the delta of', num1, 'is', delta)
+    if op == 'square root':
+        num1 = complex(input('what do you want the square root of: '))
+        print(num1 ** (1/2))
+    if op == 'absolute value':
+        num1 = float(input('what number do you want to absolute value of? (floats only): '))
+        if num1 > 0:
+            print(num1)
+        if num1 < 0:
+            print(num1 * (-1))
 
