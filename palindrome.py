d50292a527e2781bad344a908b9aecbc3a418e9c
Eric <ericliu3488@gmail.com>
2023-12-01 10:17:56 -0800

    Add.

--- palindrome.py
+++ palindrome.py
@@ -3,15 +3,20 @@ palindrome = str(input('enter your palindrome: '))
 small_index = 0
 large_index = -1
 
-for i in range(len(palindrome)//2):
-    
-    if palindrome[small_index] != palindrome[large_index]:
-        print('this is not a palindrome')
-        break
+def palindromer(palindrome):
+    for i in range(len(palindrome)//2):
+       
+        small_index = 0
+        large_index = -1
+        
+        if palindrome[small_index] != palindrome[large_index]:
+            print('this is not a palindrome')
+            break
 
-    elif palindrome[small_index] == palindrome[large_index]:
-        small_index += 1
-        large_index = large_index - 1
-        print('this is a palindrome')
-        break
+        elif palindrome[small_index] == palindrome[large_index]:
+            small_index += 1
+            large_index = large_index - 1
+            print('this is a palindrome')
+            break
 
+palindromer(palindrome)
