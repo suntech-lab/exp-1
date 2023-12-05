920daccb9fc55038fb8f6ffd6287d02e9bf4ac68
Eric <eric@suntech.com>
2023-12-05 15:34:08 -0800

    Add.

--- isitprime.py
+++ isitprime.py
@@ -2,15 +2,15 @@ import math
 
 def is_it_prime(prime_number):
     if prime_number <= 1:
-        print('Not a prime number')
+        print('False')
         return
 
     for x in range(2, int(math.sqrt(prime_number)) + 1):
         if prime_number % x == 0:
-            print('Not a prime number')
+            print('False')
             return
 
-    print('It is a prime number')
+    print('True')
 
-prime_number = int(input('What is your number: '))
+prime_number = int(input('what is your number: '))
 is_it_prime(prime_number)
