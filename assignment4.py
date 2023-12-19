0fbb053f0862a66b780d9716d8ab6613a2114d7f
Eric <ericliu3488@gmail.com>
2023-12-19 10:06:00 -0800

    Add.

--- assignment4.py
+++ assignment4.py
@@ -1,48 +1,35 @@
-# calculates the inputted expression, prints error if it cannot
-def validate_input(input):
+def validate_input(input): # calculates the inputted expression, prints error if it cannot
 
-    # tries to evaluate the x, the user input
-    try:
+    try: # tries to evaluate the x, the user input
         num = eval(input)
 
-    # if it cant, it prints an error
-    except Exception as error:
+    except Exception as error: # if it cant, it prints an error
         print(error)
         return
     
-    # gives the evaluated input back as n
-    return num
+    return num # gives the evaluated input back as num
 
-# adds two numbers together, one of the simplest functions
-def add():
+def add(): # adds two numbers together, one of the simplest functions
     
-    # ask user for input
-    print('enter an integer, float, or expression')
+    print('enter an integer, float, or expression') # ask user for input
     num1 = input()
     
-    # validates input
-    num1 = validate_input(num1)
+    num1 = validate_input(num1) # validates input
     if not num1:
         return
     
-    # ask user for second input
-    print('enter your second integer, float, or expression')
+    print('enter your second integer, float, or expression') # ask user for second input
     num2 = input()
 
-    # validate second input
-    num2 = validate_input(num2)
+    num2 = validate_input(num2) # validate second input
 
-    # if num2 is not valid, return
-    if not num2:
+    if not num2: # if num2 is not valid, return
         return
     
-    # once all of the inputs are validated, it does the actual operation
-    print(f'result: {num1} + {num2} = {num1 + num2}')
+    print(f'result: {num1} + {num2} = {num1 + num2}') # once all of the inputs are validated, it does the actual operation
 
-# subtracts two numbers, again one of the simplest functions
-def sub():
+def sub(): # subtracts two numbers, again one of the simplest functions
 
-    
     print('enter an integer, float, or expression')
     num1 = input()
 
@@ -59,8 +46,7 @@ def sub():
     
     print(f'result: {num1} - {num2} = {num1 - num2}')
 
-# again, one of the simplest functions, it multiplies two numbers
-def multiply():
+def multiply(): # again, one of the simplest functions, it multiplies two numbers
 
     print('enter an integer, float, or expression')
     num1 = input()
@@ -78,39 +64,33 @@ def multiply():
     
     print(f'result: {num1} * {num2} = {num1 * num2}')
 
-# divides two numbers, makes sure that it cant divide by zero
-def divide():
+def divide(): # divides two numbers, makes sure that it cant divide by zero
 
-    # asks for first input, can be any integer, float, or expression
-    print('enter an integer, float, or expression')
+    print('enter an integer, float, or expression') # asks for first input, can be any integer, float, or expression
     num1 = input()
 
     num1 = validate_input(num1)
     if not num1:
         return
     
-    # asks for the second input
-    print('enter your second integer, float, or expression that does not represent or equal to zero.')
+    print('enter your second integer, float, or expression that does not represent or equal to zero.') # asks for the second input
+
     num2 = input()
 
-    # validates it as if it were any other input
-    num2 = validate_input(num2)
+    num2 = validate_input(num2) # validates it as if it were any other input
 
-    # this time, the second input may not equal zero. if it does, it prints a request to enter it again
-    while num2 == 0:
+    while num2 == 0: # this time, the second input may not equal zero. if it does, it prints a request to enter it again
         print('num2 is equal to or represents zero, please input another number.')
         num2 = input()
 
-        # validates the new input
-        num2 = validate_input(num2)
+        num2 = validate_input(num2) # validates the new input
     
     if not num2:
         return
     
     print(f'result: {num1} / {num2} = {num1 / num2}')
 
-# mods the two inputted numbers so long as the second one is not zero.
-def mod():
+def mod(): # mods the two inputted numbers so long as the second one is not zero.
 
     print('enter an integer, float, or expression')
     num1 = input()
@@ -119,26 +99,22 @@ def mod():
     if not num1:
         return
     
-    # enter the second input, like any other function
-    print('enter your second integer, float, or expression')
+    print('enter your second integer, float, or expression') # enter the second input, like any other function
     num2 = input()
     num2 = validate_input(num2)
 
-    # this time, the second input may not equal zero. if it does, it prints a request to enter it again
-    while num2 == 0:
+    while num2 == 0: # this time, the second input may not equal zero. if it does, it prints a request to enter it again
         print('num2 is equal to or represents zero, please input another number.')
         num2 = input()
 
-        # validates the new input
-        num2 = validate_input(num2)
+        num2 = validate_input(num2) # validates the new input
 
     if not num2:
         return
     
     print(f'result: {num1} % {num2} = {num1 % num2}')
 
-# finds the greatest common divisor of two numbers
-def gcd():
+def gcd(): # finds the greatest common divisor of two numbers
 
     print('enter an integer, float, or expression')
     num1 = input()
@@ -154,19 +130,15 @@ def gcd():
     if not num2:
         return
     
-    # keeps the original inputs, because it modifies them later
     og_num1 = num1
-    og_num2 = num2
+    og_num2 = num2 # keeps the original inputs, because it modifies them later
 
-    # modifies the inputs to find the gcd with euclid's algorithm
     while num2 != 0:
-        num1, num2 = num2, num1 % num2
+        num1, num2 = num2, num1 % num2 # modifies the inputs to find the gcd with euclid's algorithm
 
-    # prints the original inputs, and then the number that was converted to the gcd
-    print(f'result: the gcd of {og_num1} and {og_num2} = {num1}')
+    print(f'result: the gcd of {og_num1} and {og_num2} = {num1}') # prints the original inputs, and then the number that was converted to the gcd
 
-# gets the lowest common multiple of two user inputs
-def lcm():
+def lcm(): # gets the lowest common multiple of two user inputs
 
     print('enter an integer, float, or expression')
     num1 = input()
@@ -182,20 +154,16 @@ def lcm():
     if not num2:
         return
     
-    # finds the gcd between the two numbers
     og_num1 = num1
     og_num2 = num2
     while num2 != 0:
-        num1, num2 = num2, num1 % num2
+        num1, num2 = num2, num1 % num2 # finds the gcd between the two numbers
 
-    # floor divides the product of the original inputs by the gcd to find the lcm
-    solution = (og_num1 * og_num2) // num1
+    solution = (og_num1 * og_num2) // num1 # floor divides the product of the original inputs by the gcd to find the lcm
 
-    # prints the solution
-    print(f"result: The lcm of {og_num1} and {og_num2} is: {solution}")
+    print(f"result: The lcm of {og_num1} and {og_num2} is: {solution}") # prints the solution
 
-# raises the first input to the power of the second
-def power():
+def power(): # raises the first input to the power of the second
 
     print('enter an integer, float, or expression')
     num1 = input()
@@ -211,20 +179,15 @@ def power():
     if not num2:
         return
     
-    # prevents outputting complex numbers
-    if type(num1 ** num2) == complex:
+    if type((num1) ** (num2)) == complex: # prevents outputting complex numbers
         print('complex number, not available')
     
-    # if there is no complex number output, print the answer.
-    else:
+    else: # if there is no complex number output, print the answer
         print(f'result: {num1} ** {num2} = {num1 ** num2}')
 
+def factorial(): # finds the factorial of a positive integer
 
-# finds the factorial of a positive integer
-def factorial():
-
-    # the factorial of negative numbers are complex
-    print('enter a positive integer')
+    print('enter a positive integer') # the factorial of negative numbers are complex
     num1 = input()
 
     num1 = validate_input(num1)
@@ -234,30 +197,23 @@ def factorial():
     if num1 == 0 or num1 == 1: # base case for 0 and 1
         return 1
     
-    # if the inputted number is less than 0, it is a negative, and the result is complex
-    elif num1 < 0:
+    elif num1 < 0: # if the inputted number is less than 0, it is a negative, and the result is complex
         print('complex number, not available.')
         return
 
-    # prevent factorial for decimal numbers
-    elif type(num1) == float:
+    elif type(num1) == float: # prevent factorial for decimal numbers
         print('factorial does not exist for decimal numbers')
 
-    # if it is larger than 0, proceed with calculating the factorial
-    else:
+    else: # if it is larger than 0, proceed with calculating the factorial
 
-        # multiply each number from 2 to the input
         result = 1
 
-        # 1 is added to the first input because it is exclusive
-        for i in range(2, num1 + 1):
+        for i in range(2, num1 + 1): # 1 is added to the first input because it is exclusive
             result *= i
 
-        # print the result
-        print(f'result: the factorial of {num1} = {result}')
+        print(f'result: the factorial of {num1} = {result}') # print the result
 
-# square root the input, or raise it to the power of 1/2
-def sqrt():
+def sqrt(): # square root the input, or raise it to the power of 1/2
 
     print('enter an integer, float, or expression')
     num1 = input()
@@ -266,16 +222,13 @@ def sqrt():
     if not num1:
         return
     
-    # cannot square negative numbers, so print that complex numbers are not available
-    if num1 < 0:
+    if num1 < 0: # cannot square negative numbers, so print that complex numbers are not available
         print('complex number, not available.')
 
-    # otherwise if it is not a complex number, proceed with calculating it
-    else:
+    else: # otherwise if it is not a complex number, proceed with calculating it
         print(f'result: the square root of {num1} = {num1 ** (1/2)}')
 
-# the 'menu' that shows the user what can be inputted
-operation_schema = [
+operation_schema = [ # the 'menu' that shows the user what can be inputted
     {'operation': '1', 'description': 'addition', 'func': add},
     {'operation': '2', 'description': 'subtraction', 'func': sub},
     {'operation': '3', 'description': 'multiplication', 'func': multiply},
