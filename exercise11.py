8c74d2b73a7db926fe9b6acf501317d19d88d507
Eric <ericliu3488@gmail.com>
2024-02-23 11:58:46 -0800

    Add.

--- exercise11.py
+++ exercise11.py
@@ -4,7 +4,7 @@ def pass_gen():
 
     password_list = []
 
-    while len(password_list) < random.randint(7, 11):
+    while len(password_list) < 100:
         password_list.append(chr(random.randint(33, 127)))
         password = ''.join(password_list)
         
