ce180dd7b9ab054cf54982c60850162f06c22996
Eric <ericliu3488@gmail.com>
2024-02-27 10:21:48 -0800

    Add.

--- poopoo.py
+++ poopoo.py
@@ -1,18 +1,4 @@
-names = ""
+string = 'monkeys'
+d = 'asdh'
 
-accumulator_variable = 0
-
-while True:
-
-    name = str(input('Enter a Name: '))
-
-    if name == '':
-        print(f'Your names: {names}')
-        break
-
-    if accumulator_variable == 0:
-        names += name
-        accumulator_variable = 1
-
-    elif accumulator_variable == 1:
-        names += ', ' + name
+print(string + ' ' + d)
