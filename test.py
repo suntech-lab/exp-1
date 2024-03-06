6deccddc962af6f87a438d0c3d67a51d947ce18c
Eric <ericliu3488@gmail.com>
2024-03-06 08:32:17 -0800

    Add.

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print(float(36/38))
+from urllib.request import urlopen
+import json
+
+url = 'https://jsonplaceholder.typicode.com/todos/1'
+with urlopen(url) as response:
+    body = response.read()
+
+todo_item = json.loads(body)
+print(todo_item)
