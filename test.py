2baf213244ef705f515e6733fbfc53e12c5c86be
Eric <ericliu3488@gmail.com>
2024-03-07 10:07:23 -0800

    Add.

--- test.py
+++ test.py
@@ -1,9 +1,75 @@
-from urllib.request import urlopen
-import json
+import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 
-url = 'https://jsonplaceholder.typicode.com/todos/1'
-with urlopen(url) as response:
-    body = response.read()
+pygame.init()
 
-todo_item = json.loads(body)
-print(todo_item)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -1)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 1)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-1, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(1, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+
+player = Player()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
+        elif event.type == QUIT:
+            running = False
+
+    pressed_keys = pygame.key.get_pressed()
+
+    player.update(pressed_keys)
+
+    screen.fill((130, 140, 125))
+
+    surf = pygame.Surface((1000, 50))
+
+    surf.fill((200, 150, 0))
+
+    rect = surf.get_rect()
+
+    screen.blit(surf, (0, 550))
+    screen.blit(player.surf, player.rect)
+    pygame.display.flip()
+
+running = False
