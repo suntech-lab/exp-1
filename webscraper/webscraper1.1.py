a02f325b665fa4341872400360fc144f6da5f4ef
Eric <eric@suntech.com>
2024-04-03 21:40:24 -0700

    Add.

--- webscraper/webscraper1.1.py
+++ webscraper/webscraper1.1.py
@@ -3,4 +3,4 @@ import requests #requests library
 URL = "https://www.youtube.com/" #the site that the get method will use
 page = requests.get(URL) #the get method is used to get information from a specified URL, like HTML, images, or videos
 
-print(page) #print response
+print(page.content) #print response
