68e087434087e023d5d5f84822a755826dc92574
Eric <ericliu3488@gmail.com>
2024-04-05 23:32:57 -0700

    Add.

--- webscraper/webscraper1.1.py
+++ webscraper/webscraper1.1.py
@@ -1,6 +1,17 @@
-import requests #requests library
+#very simple response getter
+import requests
 
-URL = "https://www.youtube.com/" #the site that the get method will use
-page = requests.get(URL) #the get method is used to get information from a specified URL, like HTML, images, or videos
+URL = "https://google.com"
+page = requests.get(URL)
 
-print(page) #print response
+print(page)
+
+#authentication
+from requests.auth import HTTPBasicAuth
+
+page = requests.get('https://httpbin.org/basic-auth/user/pass') #trying to get a response with no authentication
+print(page) #this will print the 401 response code instead of the 200 response code because it lacks valid authentication
+
+log_in = HTTPBasicAuth('user', 'pass') #the username and password, trying to validate authentication
+page = requests.get('https://httpbin.org/basic-auth/user/pass', auth=log_in)
+print(page) #prints a 200 response code this time
