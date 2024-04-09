bf6dc2485c3621514ef8f9f05da14308b61993a3
Eric <ericliu3488@gmail.com>
2024-04-09 11:05:27 -0700

    Add.

--- webscraper/stocktest.py
+++ webscraper/stocktest.py
@@ -4,7 +4,8 @@ it finds the current price of the S&P 500 and the price change
 uses the yahoo finance website
 ill test out how it fares when the price changes, when it changes
 '''
-
+import time
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
@@ -21,6 +22,9 @@ price = where_its_at.find('fin-streamer')
 stock_price = price.text
 stock_increase = price_element.text
 
-print('Stock Increase:', stock_increase)
-print('Current Stock Value:', stock_price)
-
+while True:
+    time.sleep(600)
+    print('Stock Increase:', stock_increase)
+    print('Current Stock Value:', stock_price)
+    print(datetime.now())
+    print()
