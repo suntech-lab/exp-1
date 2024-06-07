4dbf1954a251abbb1e2e6d7aaf3b175e6f1f4bcc
Eric <ericliu3488@gmail.com>
2024-06-07 08:22:56 -0700

    recursion pract

---
+++ school/exampractice/6recursion.py
@@ -0,0 +1,11 @@
+x = int(input('what is your lower limit: '))
+y = int(input('what is your upper limit: '))
+
+def sigma(x, y):
+    
+    if y == x:
+        return y
+    
+    return y + sigma(x, y-1)
+
+print(sigma(x, y))
