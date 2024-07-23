2145d6b0b02cb5f128ac9158d2fedbe1d26d046d
Eric <eric@suntech.com>
2024-07-22 18:55:13 -0700

    linkedlist

--- leet/experimentation/linkedlists2.py
+++ leet/experimentation/linkedlists2.py
@@ -1,14 +1,14 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, val):
+        self.val = val
         self.next = None
 
 class LinkedList:
     def __init__(self):
         self.head = None
     
-    def append(self, data):
-        new_node = Node(data)
+    def append(self, val):
+        new_node = Node(val)
         
         if self.head is None:
             self.head = new_node
@@ -22,25 +22,16 @@ class LinkedList:
         current = self.head
 
         while current:
-            print(current.data, end=' -> ')
+            print(current.val, end=' -> ')
             current = current.next
         print('None')
-
-    def values(self):
-        list_of_values = []
-        current = self.head
-
-        while current:
-            list_of_values.append(current.data)
-            current = current.next
-        return list_of_values
     
-    def delete(self, data):
+    def delete(self, val):
         current = self.head
         previous = None
 
         while current:
-            if current.data == data:
+            if current.val == val:
                 if previous is None:
                     self.head = current.next
                 else:
@@ -48,6 +39,31 @@ class LinkedList:
                 return
             previous = current
             current = current.next
+
+    def values(self):
+        list_of_values = []
+        current = self.head
+
+        while current:
+            list_of_values.append(current.val)
+            current = current.next
+        return list_of_values
+    
+    def addition(linkedlist1, linkedlist2):
+        linkedlist1 = LinkedList()
+        linkedlist2 = LinkedList()
+
+        unlinkedlist1 = linkedlist1.values()
+        unlinkedlist2 = linkedlist2.values()
+
+        unlinkedlist1 = int(''.join(str(x) for x in unlinkedlist1))
+        unlinkedlist2 = int(''.join(str(x) for x in unlinkedlist2))
+
+        sum = str(unlinkedlist1 + unlinkedlist2)
+        result = []
+        for x in sum:
+            result.append(int(x))
+        return result
 
 '''
 get values of data,
