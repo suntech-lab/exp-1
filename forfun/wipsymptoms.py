da2b075fc2d1355c2f32b0ec41d623c5e1dd0c34
Eric <ericliu3488@gmail.com>
2024-10-15 07:53:56 -0700

    asda

--- forfun/wipsymptoms.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://en.wikipedia.org/wiki/List_of_medical_symptoms'
-response = requests.get(url)
-soup = BeautifulSoup(response, "html.parser")
-print(soup)
