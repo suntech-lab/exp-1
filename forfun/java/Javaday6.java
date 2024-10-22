b794e9d2152e6d0f99c458c98ca38b7dfcbae604
Eric <ericliu3488@gmail.com>
2024-10-22 08:07:40 -0700

    javada6 started

---
+++ forfun/java/Javaday6.java
@@ -0,0 +1,13 @@
+package forfun.java;
+public class Javaday6{
+    int x;
+
+    public Javaday6(){
+        x = 5;
+    }
+
+    public static void main(String[] args){
+        Javaday6 myObj = new Javaday6();
+        System.out.println(myObj.x);
+    }
+}
