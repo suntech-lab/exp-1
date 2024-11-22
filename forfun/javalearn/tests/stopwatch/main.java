264953e76e18f328897cb8921cf9270ad80bb6b4
Eric <eric@suntech.com>
2024-11-21 21:55:31 -0800

    afsfsaa

---
+++ forfun/javalearn/tests/stopwatch/main.java
@@ -0,0 +1,9 @@
+public class Stopwatch{
+    public static void main(String[] args){
+        int initialT = 0;
+        while(true){
+            System.out.println(initialT);
+            initialT++;
+        }
+    }
+}
