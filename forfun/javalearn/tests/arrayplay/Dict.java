cc7234f022a03b5735e8d5ac3370c4da68e86cb1
Eric <ericliu3488@gmail.com>
2024-11-29 08:14:46 -0800

    asdasd

---
+++ forfun/javalearn/tests/arrayplay/Dict.java
@@ -0,0 +1,11 @@
+package javalearn.tests.arrayplay;
+
+public class Dict {
+    public static void main(String[] args){
+        int[] x = {1, 2, 3};
+        int[] y = {1, 2, 3};
+        int[][] matrix = {x, y};
+
+        System.out.println(matrix[1][1]);
+    }
+}
---
+++ forfun/javalearn/tests/arrayplay/Goofyarray.java
@@ -0,0 +1,18 @@
+package javalearn.tests.arrayplay;
+
+import java.util.Arrays;
+
+public class Goofyarray {
+    public static void main(String[] args){
+        int[] ages = {15, 16, 21};
+        int currentyear = 2024;
+        int year = 2026;
+        int i = 0;
+
+        for(i = 0; i < ages.length; i++){
+             ages[i] += year - currentyear;
+        }
+
+        System.out.println(Arrays.toString(ages));
+    }   
+}
