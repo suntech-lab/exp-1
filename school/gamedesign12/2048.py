571a22fa622f26e46bcc0819b08b00018577206e
Eric <ericliu3488@gmail.com>
2025-02-05 08:54:52 -0800

    nim better

--- school/gamedesign12/2048.py
+++ school/gamedesign12/2048.py
@@ -6,18 +6,63 @@ Eric Liu
 
 import random
 
-board = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 2, 0, 0],
-        [0, 0, 0, 0]
-    ]
+# Create a 4x4 game grid to store the current state of the game board. All values start at 0
+board = [[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0]]
 
-def Display(board):
+# Generate a single 2 at a random position
+def AddRandomTwo(board):
+    emptyCells = [(i, j) for i in range(4) for j in range(4) if board[i][j] == 0]
+    if emptyCells:
+        i, j = random.choice(emptyCells)
+        board[i][j] = 2
 
-    i = 0
-    while i < len(board):
-        print(board[i])
-        i += 1
+def Swap(value1, value2):
+    return value2, value1
 
-Display(board)
+def MergeRowLeft(row):
+    for i in range(3):
+        if row[i] == row[i + 1]:
+            row[i], row[i + 1] = Swap(row[i], row[i + 1])
+            row[i] *= 2
+            row[i + 1] = 0
+
+        
+    return row
+
+def CompactRow(row):
+    for i in range(3):
+        if row[i] == 0:
+            if row[i + 1] != 0:
+                row[i], row[i + 1] = Swap(row[i], row[i + 1])
+
+def PrintBoard(board):
+    # Pre: needs the board to be passed in
+    # Post: will print the board to the console
+
+    for row in board:
+        print('+------' * 4 + '+')
+        for cell in row:
+            print(f'| {cell if cell != 0 else "":4}', end=' ')
+        print('|')
+    print('+------' * 4 + '+')
+
+
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+AddRandomTwo(board)
+
+PrintBoard(board)
+MergeRowLeft(board[0])
+PrintBoard(board)
--- school/gamedesign12/GameOfNim.py
+++ school/gamedesign12/GameOfNim.py
@@ -3,176 +3,219 @@ Game Of Nim GameDesign12
 Eric Liu
 30/01/2025
 '''
-#imports
+
 import random
 import time
+import os
 
-#nobody wants to play a game where the turn order is the same EVERY TIME
+#functions
+def prRed(redText):
+    print("\033[91m {}\033[00m" .format(redText))
+
+def prGreen(greenText):
+    print("\033[92m {}\033[00m" .format(greenText))
+
+def prPurple(purpleText):
+    print("\033[95m {}\033[00m" .format(purpleText))
+
+def prHighlightText(hightlightedText):
+    print("\033[41m {}\033[0m" .format(hightlightedText))
+
+
 def turnOrder():
 
-    #coin flip to determine who goes first
+    #Pre: needs the random module and needs to be placed at the start of the game
+    #Post: determines the turn order of the game
+
     playerOrComputer = random.randint(0, 1)
 
     if playerOrComputer == 1:
         print('You go first!')
+        
         return True
     
     else:
         print('The computer goes first!')
+        
         return False
 
-#the computer needs to take 1-3 stones to progress the game
-def stonesComputerWillTake(stonesLeft, pileOfStones, stonesTaken):
 
-    #this is here to make sure the game doesn't flash when the computer takes stones
-    time.sleep(2)
+def makeComputerTakeStones(stonesLeft, pileOfStones, stonesTaken):
 
-    #this is the computer's strategy to win the game
-    stonesComputerTakes = (stonesLeft % 4) - 1
+    #Pre: needs to know how many stones are left, the pile of stones,
+    #how many stones have been taken, and the time module
+    #Post: the computer will take stones from the pile
 
-    #this will make the computer take one stone only if it's not on the winning track
-    if stonesComputerTakes <= 0:
-        stonesComputerTakes = 3
+    time.sleep(3)
 
-    #this tells the program how many stones are available for taking
-    stonesLeft -= stonesComputerTakes
+    stonesComputerWillTake = (stonesLeft % 4) - 1
 
-    #the pile needs to update when the computer takes stones
-    while stonesComputerTakes > 0:
+    #this will make the computer take three stones only if it's not on the winning track
+    if stonesComputerWillTake <= 0:
+        stonesComputerWillTake = 3
+
+    stonesLeft -= stonesComputerWillTake
+
+    while stonesComputerWillTake > 0:
         pileOfStones[stonesTaken] = ['[STONE]', '       ', '       ']
+        
         stonesTaken += 1
-        stonesComputerTakes -= 1
+        
+        stonesComputerWillTake -= 1
 
     return pileOfStones, stonesLeft, stonesTaken
 
-#the user needs to take 1-3 stones to progress the game
+
 def stoneTakingPrompt(stonesLeft, pileOfStones, stonesTaken):
+
+    #Pre: needs to know how many stones are left, the pile of stones, and how many stones have been taken
+    #Post: will ask the user to take stones from the pile
+
     stonesUserTakes = 0
     
-    #stonesUserTakes starts at zero, so this loop will run at least once
-    while stonesUserTakes < 1 or stonesUserTakes > 3:
+    try:
+        stonesUserTakes = int(input('How many stones do you want to take? (1-3 stones only): '))
+        
+        if int(stonesUserTakes) < 1 or int(stonesUserTakes) > 3:
+            print("Your integer is out of range. Try again.")
+        
+            stoneTakingPrompt(stonesLeft, pileOfStones, stonesTaken)
 
-        #this try except block is here to catch any strings or special characters that the user might input
-        try:
-            stonesUserTakes = int(input('How many stones do you want to take? (1-3 stones only): '))
+    except:
+        print("Don't put any letters or special characters. Try again.")
+        
+        stoneTakingPrompt(stonesLeft, pileOfStones, stonesTaken)
 
-            #if the user's input is out of range, this will tell the user to try again
-            if int(stonesUserTakes) < 1 or int(stonesUserTakes) > 3:
-                print("Your integer is out of range. Try again.")
-
-        except:
-            print("Don't put any letters or special characters. Try again.")
-
-
-    #this tells the program how many stones are available for taking
     stonesLeft -= stonesUserTakes
 
-    #this updates the pile when the user takes stones
     while stonesUserTakes > 0:
         pileOfStones[stonesTaken] = ['       ', '       ', '[STONE]']
+        
         stonesTaken += 1
+        
         stonesUserTakes -= 1
 
-    #this returns all the needed variables for the game to continue
     return pileOfStones, stonesLeft, stonesTaken
 
-#this is what actually prints the pile of stones and tells the user how many stones are left
+
 def showPile(pileOfStones, stonesLeft):
+
+    #Pre: needs to have the pile of stones and know how many stones are left
+    #Post: will display the pile of stones and how many stones are left
+
     for i in pileOfStones:
         print(i[0], i[1], i[2])
 
-    print(stonesLeft, 'stone(s) left.')
+    prRed(f'{stonesLeft} stone(s) left.')
 
-#this determines if the player has lost
+
 def determineWinner(stonesLeft, pileOfStones):
+
+    #Pre: needs to know whose turn it is, how many stones are left, and the pile of stones
+    #Post: will determine the winner of the game
+
     if stonesLeft <= 1:
-        
-        #this will print a message depending on whose turn it is when the game ends
-        if playerTurn:
+        if playerTurn: #this will print a message depending on whose turn it is when the game ends
             showPile(pileOfStones, stonesLeft)
-            print("""
+            
+            prRed("""
+     You take the last stone...
 .    __                         __.
 .   / /   ____  ________  _____/ /.
 .  / /   / __ \/ ___/ _ \/ ___/ / .
 . / /___/ /_/ (__  )  __/ /  /_/  .
 ./_____/\____/____/\___/_/  (_)   .
                   """)
+            
             exit()
         
         else:
             showPile(pileOfStones, stonesLeft)
-            print("""
+            
+            prGreen("""
+   The computer takes the last stone...
 .__  __                        _       __.
 .\ \/ /___  __  __   _      __(_)___  / /.
 . \  / __ \/ / / /  | | /| / / / __ \/ / .
 . / / /_/ / /_/ /   | |/ |/ / / / / /_/  .
 ./_/\____/\____/    |__/|__/_/_/ /_(_)   .
                   """)
+            
             exit()
     
-#this makes sure the game doesn't jumpscare the user when they run the program
+
 def beginGame():
+
+    #Pre: it doesn't need anything except being put at the initial start of the game
+    #Post: will start the game if the player wants to play, otherwise exits the program
+
     beginQuery = input('Would you like to play? (y/n): ')
     
-    #this if else block will handle both easier than a while loop because there's only two options
     if beginQuery == 'y':
         return True
     
     elif beginQuery == 'n':
         print('Goodbye!')
+        
         exit()
     
     else:
         print('Invalid input. Try again.')
+        
         beginGame()
+    
 
-#initialize how many stones are in the pile
-stonesLeft = random.randint(18, 26)
+def printTitle():
+    
+    #Pre: doesn't need anything
+    #Post: will print the title of the game
 
-#initialize the pile of stones
-pileOfStones = []
-
-for i in range(stonesLeft):
-    pileOfStones.append(['       ', '[STONE]', '       '])
-
-stonesTaken = 0
-
-#introduce the player to the game
-print("""
+    prPurple("""
 .   ______                        ____  ____   _   ___         .
 .  / ____/___ _____ ___  ___     / __ \/ __/  / | / (_)___ ___ .
 . / / __/ __ `/ __ `__ \/ _ \   / / / / /_   /  |/ / / __ `__ \.
 ./ /_/ / /_/ / / / / / /  __/  / /_/ / __/  / /|  / / / / / / /.
 .\____/\__,_/_/ /_/ /_/\___/   \____/_/    /_/ |_/_/_/ /_/ /_/ .
+          
+   AI      vs     YOU
       """)
-print("""
+
+
+#initializing the game
+printTitle()
+prGreen("""
 Welcome to the Game of Nim! The rules are simple:
 Take turns taking 1-3 stones from the pile
 The player who takes the last stone loses! Good luck!
+(make sure your terminal really tall so you can see the whole pile)
 """)
+stonesLeft = random.randint(18, 26)
+pileOfStones = []
+for i in range(stonesLeft):
+    pileOfStones.append(['       ', '[STONE]', '       '])
 
-#ask the user to play the game
-beginGame()
-
-#turnOrder() returns true or false on a coin flip, telling the program 
+stonesTaken = 0
 playerTurn = turnOrder()
+beginGame()
 
 #main loop
 while True:
 
-    #determineWinner() will end the game if the player has lost, so there isn't a turn after the game ends
+    os.system('cls')
+
+    printTitle()
+
     determineWinner(stonesLeft, pileOfStones)
 
-    #showPile() is here because it's the first thing the user sees when it's their turn or the computer's turn
     showPile(pileOfStones, stonesLeft)
+    
+    if not playerTurn:
+        prHighlightText('The computer takes a stone or two... or three...')
 
-    #this switches depending on playerTurn
     if playerTurn:
         pileOfStones, stonesLeft, stonesTaken = stoneTakingPrompt(stonesLeft, pileOfStones, stonesTaken)
 
     else:
-        pileOfStones, stonesLeft, stonesTaken = stonesComputerWillTake(stonesLeft, pileOfStones, stonesTaken)
-        print('The computer takes a stone or two... or three...')
+        pileOfStones, stonesLeft, stonesTaken = makeComputerTakeStones(stonesLeft, pileOfStones, stonesTaken)
 
-    #switch the actual turn
     playerTurn = not playerTurn
